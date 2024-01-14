dfcac2283bc634ca294d77c388f1dd2e89cf934e
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-01-14 23:39:51 +0300

    feat: do the task 'write file' but the output for the Ctrl+C command is not quite correct

---
+++ 02-write-file/index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const file = './02-write-file/your-text.txt';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+fs.writeFile(file, '', (err) => {
+  if (err) {
+    console.error('Error! File was not created!', err);
+  } else {
+    console.log('File created successfully!');
+    rl.setPrompt('Enter text or press "Ctrl + C" or type "exit" to quit: ');
+    rl.prompt();
+  }
+});
+
+const writeToFile = (text) => {
+  fs.appendFile(file, text + '\n', (err) => {
+    if (err) {
+      console.error('Error! Your text was not written into the file!', err);
+    } else {
+      console.log('Text is written to file successfully!');
+      rl.prompt();
+    }
+  });
+};
+
+rl.on('SIGINT', () => {
+  console.log('It was a great job!');
+  rl.close();
+});
+
+rl.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    console.log('It was a great job!');
+    rl.close();
+  } else {
+    writeToFile(input);
+  }
+});
