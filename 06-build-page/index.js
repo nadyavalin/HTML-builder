d6c9f00be719f46665dcc137bf83cb5eecee9272
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-01-18 22:29:27 +0300

    feat: make code to create index.html in project-dist folder with header, section and footer components from components folder

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,14 +1,54 @@
 const fs = require('fs');
-// const path = require('path');
+const path = require('path');
 
-// const componentsFolder = 'components';
-// const stylesFolder = 'styles';
-// const assetsFolder = 'assets';
-const projectDistFolder = './06-build-page/project-dist';
+// Создание папки project-dist
+const projectDistFolderPath = './06-build-page/project-dist';
 
-fs.mkdir(projectDistFolder, { recursive: true }, (err) => {
+fs.mkdir(projectDistFolderPath, { recursive: true }, (err) => {
   if (err) {
     console.error('Error creating project-dist folder:', err);
   }
   console.log('Project-dist folder has been created!');
+});
+
+// Создание index.html в папке project-dist
+const templatePath = './06-build-page/template.html';
+fs.readFile(templatePath, 'utf8', (readFileErr, templateContent) => {
+  if (readFileErr) {
+    console.error('Error reading template file:', readFileErr);
+    return;
+  }
+  const templateTags = templateContent.match(/{{\w+}}/g);
+  if (templateTags) {
+    templateTags.forEach((tag) => {
+      const componentName = tag.replace(/{{|}}/g, '');
+      const componentPath = path.join(
+        './06-build-page/components',
+        componentName + '.html',
+      );
+      fs.readFile(
+        componentPath,
+        'utf8',
+        (readComponentErr, componentContent) => {
+          if (readComponentErr) {
+            console.error('Error reading component file:', readComponentErr);
+            return;
+          }
+          templateContent = templateContent.replace(
+            new RegExp(tag, 'g'),
+            componentContent,
+          );
+
+          const indexPath = path.join(projectDistFolderPath, 'index.html');
+          fs.writeFile(indexPath, templateContent, 'utf8', (writeFileErr) => {
+            if (writeFileErr) {
+              console.error('Error writing to index.html:', writeFileErr);
+              return;
+            }
+          });
+        },
+      );
+    });
+  }
+  console.log('index.html has been created in project-dist folder');
 });
