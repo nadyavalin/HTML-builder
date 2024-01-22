08b1835956c5a79e98795a51d7f6d7395aa01ddb
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-01-22 20:56:37 +0300

    fix: redo code using promises

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,29 +1,25 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const stylesFolder = './05-merge-styles/styles';
 const distFolder = './05-merge-styles/project-dist';
 const mergedStyles = 'bundle.css';
 
-fs.readdir(stylesFolder, (err, files) => {
-  if (err) {
-    console.error('Error reading styles folder:', err);
-    return;
-  }
-
-  const styleFiles = files.filter((file) => path.extname(file) === '.css');
-
-  let bundleContent = '';
-  styleFiles.forEach((file) => {
-    const content = fs.readFileSync(path.join(stylesFolder, file), 'utf8');
-    bundleContent += content + '\n';
-  });
-
-  fs.writeFile(path.join(distFolder, mergedStyles), bundleContent, (err) => {
-    if (err) {
-      console.error('Error writing bundle file:', err);
-      return;
-    }
+fs.readdir(stylesFolder)
+  .then((files) => {
+    const styleFiles = files.filter((file) => path.extname(file) === '.css');
+    let fileReadPromises = styleFiles.map((file) => {
+      return fs.readFile(path.join(stylesFolder, file), 'utf8');
+    });
+    return Promise.all(fileReadPromises);
+  })
+  .then((fileContents) => {
+    let bundleContent = fileContents.join('\n');
+    return fs.writeFile(path.join(distFolder, mergedStyles), bundleContent);
+  })
+  .then(() => {
     console.log('Bundle file has been created successfully!');
+  })
+  .catch((err) => {
+    console.error('Error:', err);
   });
-});
